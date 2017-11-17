2b9ac8fa2d791ba4e31de586927b528e8f607f9a
DanielRicklin <ricklin.daniel@outlook.fr>
2017-11-17 13:23:53 -0100

    mini orm correction

--- src/connection/ConnectionFactory.php
+++ src/connection/ConnectionFactory.php
@@ -27,52 +27,6 @@ class ConnectionFactory{
 
 
     public static function getConnection(){
-
             return self::$connect;
     }
-
-	/*public static function makeConnection($nomFichier){
-		
-		self::$config=parse_ini_file($nomFichier);
-		
-		if(!isset($db)){
-			
-			$host=self::$config['host'];
-			$base=self::$config['database'];
-			$user=self::$config['username'];
-			$pass=self::$config['password'];
-			$option=array(	\PDO::ATTR_PERSISTENT=>true,
-					\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-					\PDO::ATTR_EMULATE_PREPARES=> false,
-					\PDO::ATTR_STRINGIFY_FETCHES => false);
-			$dsn = "mysql:host=$host;dbname=$base";
-			
-			try{
-				self::$db=new \PDO($dsn, $user, $pass, $option);
-				return self::$db;
-			} catch(PDOExeption $e){
-				echo "Erreur !: " . $e->getMessage() . "<br/>";
-				die();
-			}
-			
-		}else{
-			return self::$db;
-		}
-	}
-
-	public static function getConnection() { 
-		$host=self::$config['host'];
-		$base=self::$config['database'];
-		$user=self::$config['username'];
-		$pass=self::$config['password'];
-		$option=array(	\PDO::ATTR_PERSISTENT=>true,
-					\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-					\PDO::ATTR_EMULATE_PREPARES=> false,
-					\PDO::ATTR_STRINGIFY_FETCHES => false);
-		if (!self::$db){
-			self::$db = new PDO("mysql:host=$host;dbname=$base", $user, $pass, $option);
-		}
-
-		return self::$db;
-	}*/
 }
